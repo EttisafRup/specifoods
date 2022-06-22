0a0ca92f60a7974af5f00c2abd024eb29fa5d0b5
EttisafRup <107711625+EttisafRup@users.noreply.github.com>
2022-06-22 14:08:41 +0600

    Add files via upload

---
+++ external/sc.js
@@ -0,0 +1,10 @@
+function no(){
+    document.getElementById('nav').style.display = 'block';
+    document.getElementById('nbt').style.display = 'none';
+    document.getElementById('navhide').style.display = 'block'
+}
+function nhide() {
+    document.getElementById('nav').style.display = 'none';
+    document.getElementById('navhide').style.display = 'none';
+    document.getElementById('nbt').style.display = 'block';
+}
