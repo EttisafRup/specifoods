65f5d4749538caae5837d7fec09bbf080e49cc20
EttisafRup <107711625+EttisafRup@users.noreply.github.com>
2022-06-22 22:46:37 +0600

    Add files via upload

--- external/sc.js
+++ external/sc.js
@@ -15,3 +15,12 @@ function alt() {
     alert('This is a Assignment Project, You cant order!');
     
 }
+
+function social() {
+    alert('Contact me [for business pruposes] -\n\n > Facebook - Xtend Ettisaf \n > Email - ettisafrup@gmail.com \n > Whatsapp/Phone - +8801731420401')
+    
+}
+
+function cdx() {
+    alert('Web Designings has been done by Xtend Ettisaf \n Banners and Posters has been done by Tawhidul Islam');
+}
